c3ee25be96e6a80fd8e85d28c760b7fd6158ec79
benjamin <bloboslorca@outlook.com>
2025-09-25 18:22:29 -0300

    listado de productos funcional y page productos agregados correctamente a la pagina principal/inicio.html

--- Fase 2/Evidencias Proyecto/TresEnUnoMain/miapp/views.py
+++ Fase 2/Evidencias Proyecto/TresEnUnoMain/miapp/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from .models import Producto
+from .models import Producto, Categoria 
 
 def inicio(request):
-    return render(request, 'miapp/inicio.html')
+    
+    productos_destacados = Producto.objects.all().order_by('id')[:3]
+    contexto = {
+        'productos_destacados': productos_destacados, 
+    }
+    return render(request, 'miapp/inicio.html', contexto)
 
 def nosotros(request):
     return render(request, 'miapp/nosotros.html')
 
-def productos(request):
-    return render(request, 'miapp/productos.html')
-
-def ventas(request):
-    return render(request, 'miapp/ventas.html')
-
 def listar_productos(request):
     productos = Producto.objects.all()
     return render(request, 'miapp/productos.html', {'productos': productos})
+
+def ventas(request):
+    return render(request, 'miapp/ventas.html')
