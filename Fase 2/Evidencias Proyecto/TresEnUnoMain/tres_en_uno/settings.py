7d3c1e8384826ae93ed8c204188b67e96d686856
benjamin <bloboslorca@outlook.com>
2025-10-07 17:18:31 -0300

    servicio de email integrado, enpoints de olvide contraseña y resetear contraseña implementados con exito

--- Fase 2/Evidencias Proyecto/TresEnUnoMain/tres_en_uno/settings.py
+++ Fase 2/Evidencias Proyecto/TresEnUnoMain/tres_en_uno/settings.py
@@ -148,3 +148,12 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
     'USER_ID_FIELD': 'id',
 }
+
+# Configuración SMTP de Gmail 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'devtestbll@gmail.com' 
+EMAIL_HOST_PASSWORD = 'jpea ghnb jnjs t1bf' # Clave de gmail
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
